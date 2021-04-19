4a9909f6f494f49bfb3b149bcaeb3ec30291e8c7
LoZack19 <giozaccaria02@gmail.com>
2021-04-19 20:18:13 +0200

    fix useradd

--- part2.sh
+++ part2.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Ansi color code variables
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+yellow="\e[0;33m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
+
 ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
 hwclock --systohc
 locale-gen
@@ -12,7 +26,7 @@ echo "$machine" >> /etc/hostname
 
 printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$machine.localdomain\t$machine" >> /etc/hosts
 
-echo "Input root password..."
+printf "${bold}${green}Input root password...${reset}\n"
 passwd
 
 pacman -S grub efibootmgr
@@ -23,17 +37,23 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "INSTALLATION TERMINATED"
 
+pacman -S networkmanager
+
 printf "Input username [Default user]: "
 read username
 username=${username:-"user"}
 
-useradd -m -G wheel -s /bin/bash $username
-echo "Input user password..."
-passwd $username
+useradd -m -G wheel -s /bin/bash "$username"
+printf "${bold}${green}Input user password...${reset}\n"
+passwd "$username"
 
 pacman -S vim nano sudo
-echo 'wheel ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
+echo 'wheel ALL=(ALL:ALL) ALL' | EDITOR='tee -a' visudo
 
-pacman -S networkmanager
+cp /root/after_reboot.sh "/home/$username/.bashrc"
 
-cp /root/after_reboot.sh /home/$username/.bashrc
+printf "[${bold}${yellow} ATTENTION ${reset}] LOG IN AS USER AFTER REBOOT! \n"
+echo "Reboot in 3 seconds"
+
+sleep 3
+reboot
